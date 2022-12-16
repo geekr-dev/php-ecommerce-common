462b00ee71c155eb8ba7d1b96face71cc309cb75
geekr <geekr@hey.com>
2022-12-16 17:11:59 +0800

    抽出公共组件成为独立仓库

---
+++ src/Ecommerce/Common/DTOs/Product/ProductData.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Ecommerce\Common\DTOs\Product;
+
+class ProductData
+{
+    public function __construct(
+        public readonly ?int $id,
+        public readonly string $name,
+        public readonly string $description,
+        public readonly float $price,
+        public readonly CategoryData $category,
+    ) {
+    }
+
+    public static function fromArray(array $data): self
+    {
+        return new static(
+            $data['id'] ?? 0,
+            $data['name'],
+            $data['description'],
+            $data['price'],
+            new CategoryData(
+                $data['category']['id'],
+                $data['category']['name'] ?? ''
+            ),
+        );
+    }
+
+    public function toArray(): array
+    {
+        return get_object_vars($this);
+    }
+}
