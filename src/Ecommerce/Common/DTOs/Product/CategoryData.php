783e45eca5da7a7ad7079ad42fe85a46c2a0e19b
geekr <geekr@hey.com>
2022-12-16 21:30:59 +0800

    DTO中对外暴露UUID而不是数据库ID

--- src/Ecommerce/Common/DTOs/Product/CategoryData.php
+++ src/Ecommerce/Common/DTOs/Product/CategoryData.php
@@ -5,14 +5,14 @@ namespace Ecommerce\Common\DTOs\Product;
 class CategoryData
 {
     public function __construct(
-        public readonly int $id,
+        public readonly string $uuid,
         public readonly string $name,
     ) {
     }
 
     public static function fromArray(array $data): self
     {
-        return new static($data['id'], $data['name']);
+        return new static($data['uuid'], $data['name']);
     }
 
     public function toArray(): array
--- src/Ecommerce/Common/DTOs/Product/ProductData.php
+++ src/Ecommerce/Common/DTOs/Product/ProductData.php
@@ -5,7 +5,7 @@ namespace Ecommerce\Common\DTOs\Product;
 class ProductData
 {
     public function __construct(
-        public readonly ?int $id,
+        public readonly string $uuid,
         public readonly string $name,
         public readonly string $description,
         public readonly float $price,
@@ -16,12 +16,12 @@ class ProductData
     public static function fromArray(array $data): self
     {
         return new static(
-            $data['id'] ?? 0,
+            $data['uuid'] ?? '',
             $data['name'],
             $data['description'],
             $data['price'],
             new CategoryData(
-                $data['category']['id'],
+                $data['category']['uuid'],
                 $data['category']['name'] ?? ''
             ),
         );
